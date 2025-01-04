3468ec8982019549fbf33008e0bf7db27160d674
Opirel <151437434+Opirel@users.noreply.github.com>
2025-01-04 12:07:36 +0200

    Delete questlog directory

--- questlog/back/base/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import PlayerCharacter, Quest
-# Register your models here.
-
-admin.site.register(PlayerCharacter)
-admin.site.register(Quest)
--- questlog/back/base/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Quest
-
-class QuestForm(forms.ModelForm):
-    class Meta:
-        model = Quest
-        fields = ['quest_name', 'description', 'pc_involved', 'is_completed', 'reward']
